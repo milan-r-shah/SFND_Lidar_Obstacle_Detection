ece1bb5c4cd689e921759ef260a42fae0a1e7d05
Milan Shah <mrshah@wpi.edu>
2020-05-30 19:01:33 -0400

    Defined 'SegmentPlane' method in processPointClouds.cpp

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -55,6 +55,23 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     auto startTime = std::chrono::steady_clock::now();
 	pcl::PointIndices::Ptr inliers;
     // TODO:: Fill in this function to find inliers for the cloud.
+    pcl::SACSegmentation<PointT> seg;
+    inliers  = new pcl::PointIndices();
+    pcl::ModelCoefficients::Ptr coefficients = new pcl::ModelCoefficients();
+    
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(maxIterations);
+    seg.setDistanceThreshold(distanceThreshold);
+
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+
+    if(inliers->indices.size() == 0) 
+    {
+        std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+    }
 
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
