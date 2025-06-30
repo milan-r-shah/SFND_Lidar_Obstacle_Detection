eb2f1e5468843901ca0a37eb0cae1581bd303c7e
Milan R. Shah <mrshah.wpi@gmail.com>
2025-06-29 23:11:34 -0400

    updated the search func & added searchHelper func

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -57,9 +57,40 @@ struct KdTree {
         insertHelper(&root, 0, point, id);
     }
 
+    void searchHelper(std::vector<float> target_point, Node* node, uint depth, float distanceTol, std::vector<int>& ids) {
+        if (node != NULL) {
+            float node_x = node->point[0];
+            float node_y = node->point[1];
+
+            float target_x = target_point[0];
+            float target_y = target_point[1];
+
+            if ((node_x >= (target_x - distanceTol) && node_x <= (target_x + distanceTol)) &&
+                (node_y >= (target_y - distanceTol) && node_y <= (target_y + distanceTol))) {
+                float distance = std::sqrt((target_x - node_x) * (target_x - node_x) + (target_y - node_y) * (target_y - node_y));
+
+                if (distance <= distanceTol) {
+                    ids.push_back(node->id);
+                }
+            }
+
+            uint cd = depth % 2;  // current dimension. 0 for x, 1 for y
+            if (target_point[cd] - distanceTol < node->point[cd]) {
+                searchHelper(target_point, node->left, depth + 1, distanceTol, ids);
+            }
+
+            if (target_point[cd] + distanceTol > node->point[cd]) {
+                searchHelper(target_point, node->right, depth + 1, distanceTol, ids);
+            }
+        }
+    }
+
     // return a list of point ids in the tree that are within distance of target
     std::vector<int> search(std::vector<float> target, float distanceTol) {
         std::vector<int> ids;
+
+        searchHelper(target, root, 0, distanceTol, ids);
+
         return ids;
     }
 };
